d2b5c4c14c5b7b72812ad6221bfc6a6e83ed7d30
Danis Sharipov <danshadance@mail.ru>
2022-12-04 15:04:16 +0600

    Задача25 решена

---
+++ task25/task25.cs
@@ -0,0 +1,12 @@
+﻿// Принимаем 2 числа (A и B)
+// Возводим А в степень В
+
+Console.WriteLine("Данная программа возводит число А в натуральную степень В");
+Console.Write("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Pow(numA, numB);
+
+Console.WriteLine("Ответ: " + res);
